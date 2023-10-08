34509212d4194740d570a536b4045864831d68d5
ImpureKin <103551819+ImpureKin@users.noreply.github.com>
2023-10-08 22:51:58 +1100

    Editing POM

--- budgetbuddy/src/main/java/database/ConnectionManager.java
+++ budgetbuddy/src/main/java/database/ConnectionManager.java
@@ -9,7 +9,7 @@ public class ConnectionManager {
         try {
             Class.forName("org.sqlite.JDBC");
             // Connection Path - NEED TO CHANGE THIS ACCORDING TO ACTUAL LOCATION. Need to figure out how to do this on WebApp side.
-            Connection con = DriverManager.getConnection("jdbc:sqlite:/home/site/wwwroot/webapps/app/BudgetBuddy.db");
+            Connection con = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Big Pops\\Desktop\\AdvSoftDev\\AdvSoftDev\\budgetbuddy\\src\\main\\webapp\\BudgetBuddy.db");
             
             System.out.println("Connection Successful");
             return con;
