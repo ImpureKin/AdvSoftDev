01b5f57dfe9c7a725d20482c4c4e5152207e3e21
haigh-cyber <haighminassian01@gmail.com>
2023-10-08 20:33:58 +1100

    Deductions

--- budgetbuddy/src/main/java/controller/DeductionsController.java
+++ budgetbuddy/src/main/java/controller/DeductionsController.java
@@ -9,6 +9,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.text.SimpleDateFormat;
@@ -33,18 +35,35 @@ public class DeductionsController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (Connection connection = ConnectionManager.getConnection()) {
+
+            HttpSession session = req.getSession();
+            Integer userId = (Integer) session.getAttribute("userId");
+
+            if(userId == null) {
+                userId = 1; // for testing purposes only, change later
+                // Handle error. E.g., redirect to login page.
+                //resp.sendRedirect("index.jsp");
+                //return;
+            }
+
             String name = req.getParameter("name");
             double amount = Double.parseDouble(req.getParameter("amount"));
-            String type = req.getParameter("type");
-            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(req.getParameter("date"));
+            String category = req.getParameter("category");
+            Date date = new SimpleDateFormat("dd-MM-yyyy").parse(req.getParameter("date"));
+            String frequency = req.getParameter("frequency");  // capture frequency from request
+            String invoiceDate = req.getParameter("invoice_date");  // capture invoice_date from request
 
             Deductions newDeduction = new Deductions();
+            newDeduction.setUserId(userId);  // set the userId
             newDeduction.setname(name);
             newDeduction.setAmount(amount);
-            newDeduction.setCategory(type);
+            newDeduction.setCategory(category);
             newDeduction.setDate(date);
+            newDeduction.setFrequency(frequency);  // set the frequency. Make sure to add this method in Deductions model.
+            newDeduction.setInvoiceDate(invoiceDate);  // set the invoice date. Make sure to add this method in Deductions model.
 
-            DeductionManager.addDeduction(connection, newDeduction);
+
+            DeductionManager.addDeduction(connection, newDeduction, userId);
 
             resp.sendRedirect("Deductions");
         } catch (Exception e) {
