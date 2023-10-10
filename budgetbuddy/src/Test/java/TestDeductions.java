0c81bed33d568bb55a743b7d06be256db9656e6b
haigh-cyber <haighminassian01@gmail.com>
2023-10-10 22:07:15 +1100

    Testing files

---
+++ budgetbuddy/src/Test/java/TestDeductions.java
@@ -0,0 +1,110 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import model.Deductions;
+import controller.DeductionsController;
+import database.DeductionManager;
+import database.ConnectionManager;
+
+public class TestDeductions {
+
+    static DeductionManager deductionManager = new DeductionManager();
+    static DeductionsController deductionsController = new DeductionsController();
+
+    static Logger logger = Logger.getLogger(TestDeductions.class.getName());
+
+
+    Connection conn;
+
+    @BeforeAll
+    public static void initialiseDatabase() {
+        try {
+            dm.resetDatabase();
+        }
+        catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed resetting Database: ", e);
+        }
+    }
+
+    @AfterEach
+    public void resetDatabase() {
+        try {
+            ConnectionManager.closeConnection(conn);
+            dm.resetDatabase();
+        }
+        catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed resetting Database: ", e);
+        }
+    }
+
+    @Test
+    public void addDeductionTest() {
+        try {
+            logger.log(Level.INFO, "Beginning Test: addDeductionTest.");
+            
+            Deductions newDeduction = new Deductions();
+            newDeduction.setUserId(1); // This is for testing, assuming there's a user with ID=1
+            newDeduction.setname("Food");
+            newDeduction.setAmount(50.0);
+            newDeduction.setCategory("Test Category");
+            newDeduction.setFrequency("Monthly");
+            java.util.Date date = new java.util.Date();
+            newDeduction.setDate(date);
+            newDeduction.setInvoiceDate("10-10-2023"); // Assuming a random date here
+
+            DeductionManager.addDeduction(conn, newDeduction, 1);
+            
+            // Check if the deduction was added
+            List<Deductions> deductionsList = DeductionManager.getAllDeductions(conn);
+            boolean foundDeduction = deductionsList.stream().anyMatch(ded -> ded.getname().equals("Test Deduction"));
+            
+            assertTrue(foundDeduction);
+            
+            logger.log(Level.INFO, "Successfully Performed Test: addDeductionTest.");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed Test: addDeductionTest", e);
+        }
+    }
+
+    @Test
+    public void retrieveAllDeductionsTest() {
+        try {
+            logger.log(Level.INFO, "Beginning Test: retrieveAllDeductionsTest.");
+
+            List<Deductions> deductionsList = DeductionManager.getAllDeductions(conn);
+            
+            assertNotNull(deductionsList);
+            assertTrue(!deductionsList.isEmpty());
+            
+            logger.log(Level.INFO, "Successfully Performed Test: retrieveAllDeductionsTest."); 
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed Test: retrieveAllDeductionsTest", e);
+        }
+    }
+
+    @Test
+    public void removeDeductionTest() {
+        try {
+            logger.log(Level.INFO, "Beginning Test: removeDeductionTest.");
+
+            // Assuming an deduction with ID 1 exists for removal
+            DeductionManager.removeDeduction(conn, 1);
+
+            // Check if the deduction was removed
+            Deductions removedDeduction = DeductionManager.getDeductionById(conn, 1); // Assuming you have such a method
+            assertTrue(removedDeduction == null);
+
+            logger.log(Level.INFO, "Successfully Performed Test: removeDeductionTest.");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed Test: removeDeductionTest", e);
+        }
+    }
+}
---
+++ budgetbuddy/src/Test/java/TestExpenses.java
@@ -0,0 +1,106 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Expenses;
+import controller.ExpensesController;
+import database.ExpenseManager;
+import database.ConnectionManager;
+
+public class TestExpenses {
+
+    static ExpenseManager ExpenseManager = new ExpenseManager();
+    static ExpensesController expensesController = new ExpensesController();
+
+    static Logger logger = Logger.getLogger(TestExpenses.class.getName());
+
+ Connection conn;
+
+    @BeforeAll
+    public static void initialiseDatabase() {
+        try {
+            dm.resetDatabase();
+        }
+        catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed resetting Database: ", e);
+        }
+    }
+
+    @AfterEach
+    public void resetDatabase() {
+        try {
+            ConnectionManager.closeConnection(conn);
+            dm.resetDatabase();
+        }
+        catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed resetting Database: ", e);
+        }
+    }
+
+    @Test
+    public void addExpenseTest() {
+        try {
+            logger.log(Level.INFO, "Beginning Test: addExpenseTest.");
+            
+            Expenses newExpense = new Expenses();
+            newExpense.setUserId(1); // This is for testing, assuming there's a user with ID=1
+            newExpense.setExpenseName("Test Expense");
+            newExpense.setAmount(100.0);
+            newExpense.setCategory("Test Category");
+            java.util.Date date = new java.util.Date();
+            newExpense.setDate(date);
+
+            database.ExpenseManager.addExpense(conn, newExpense, 1);
+            
+            // Check if the expense was added
+            List<Expenses> expensesList = database.ExpenseManager.getAllExpenses(conn);
+            boolean foundExpense = expensesList.stream().anyMatch(exp -> exp.getExpenseName().equals("Test Expense"));
+            
+            assertTrue(foundExpense);
+            
+            logger.log(Level.INFO, "Successfully Performed Test: addExpenseTest.");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed Test: addExpenseTest", e);
+        }
+    }
+
+    @Test
+    public void retrieveAllExpensesTest() {
+        try {
+            logger.log(Level.INFO, "Beginning Test: retrieveAllExpensesTest.");
+
+            List<Expenses> expensesList = database.ExpenseManager.getAllExpenses(conn);
+            
+            assertNotNull(expensesList);
+            assertTrue(!expensesList.isEmpty());
+            
+            logger.log(Level.INFO, "Successfully Performed Test: retrieveAllExpensesTest."); 
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed Test: retrieveAllExpensesTest", e);
+        }
+    }
+
+    @Test
+    public void deleteExpenseTest() {
+        try {
+            logger.log(Level.INFO, "Beginning Test: deleteExpenseTest.");
+
+            // Assuming an expense with ID 1 exists for deletion
+            database.ExpenseManager.deleteExpense(conn, 1);
+
+            // Check if the expense was deleted
+            Expenses deletedExpense = ExpenseManager.getExpenseById(conn, 1);
+            assertTrue(deletedExpense == null);
+
+            logger.log(Level.INFO, "Successfully Performed Test: deleteExpenseTest.");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed Test: deleteExpenseTest", e);
+        }
+    }
+}
